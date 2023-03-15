eddf82de599661530329ea31d3e012803ffb8f22
isaiasmanjate <imanjate@Manjate>
2023-03-15 11:45:54 +0200

    editando fornecedor parte-2

--- resources/views/suppliers/show.blade.php
+++ resources/views/suppliers/show.blade.php
@@ -1,6 +1,8 @@
 '
+
 @extends("layouts.app")
 @section("wrapper")
+
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
         <div class="breadcrumb-title pe-3">Clientes</div>
@@ -18,7 +20,11 @@
                 <a href="{{route('suppliers.edit', $supplier)}}" class="btn btn-primary">Editar</a>
                 <a href="{{route('suppliers.create')}}" class="btn btn-primary">Remover</a>
                 <a href="{{route('suppliers.create')}}" class="btn btn-primary">Adicionar</a>
-
+                @php
+                    $province = \App\Models\Province::find($supplier->supplierable->address_province_id);
+                    $country=\App\Models\Country::find($supplier->supplierable->address_country_id);
+                    $district=\App\Models\District::find($supplier->supplierable->address_district_id);
+                @endphp
             </div>
         </div>
     </div>
@@ -53,17 +59,17 @@
                                     <span class="text-secondary">{{$supplier->supplierable->nuit}}</span>
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0">nacionalidade</h6>
+                                    <h6 class="mb-0">Nacionalidade</h6>
 
-                                    <span class="text-secondary">{{$supplier->supplierable->address_country_id}}</span>
+                                    <span class="text-secondary">{{$country->name}}</span>
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0">provincia</h6>
-                                    <span class="text-secondary">{{$supplier->supplierable->address_province_id}}</span>
+                                    <h6 class="mb-0">Provincia</h6>
+                                    <span class="text-secondary">{{$province->name}}</span>
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0">nacionalidade</h6>
-                                    <span class="text-secondary">{{$supplier->supplierable->address_district_id}}</span>
+                                    <h6 class="mb-0">Distrito</h6>
+                                    <span class="text-secondary">{{$district->name}}</span>
                                 </li>
 
 
@@ -88,7 +94,7 @@
                                 </div>
                             </div>
                             <div class="mb-3 row">
-                                <label for="staticEmail" class="col-sm-3 col-form-label text-end fw-bold">Tipo de cliente</label>
+                                <label for="staticEmail" class="col-sm-3 col-form-label text-end fw-bold">Fornecedor</label>
                                 <div class="col-sm-9">
                                     <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$supplier->supplierable->name}}">
                                 </div>
