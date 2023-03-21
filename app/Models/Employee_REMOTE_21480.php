a73433812efaa04df75103e38d36af65953ac7b9
isaiasmanjate <imanjate@Manjate>
2023-03-21 09:13:19 +0200

    Adicionando imagem no coloborador e dando permissoes de cadastro

---
+++ app/Models/Employee_REMOTE_21480.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\EmployeeSaved;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    protected $dispatchesEvents = [
+        'saved' => EmployeeSaved::class,
+    ];
+
+    protected $casts = [
+        #'employee_position_id' => \App\Enums\EmployeePosition::class,
+    ];
+
+
+    public function person(){
+        return $this->belongsTo(Person::class);
+    }
+    public function department(){
+        return $this->belongsTo(Department::class);
+    }
+
+    public function contractType(){
+        return $this->belongsTo(EmployeeContractType::class);
+    }
+
+    public function contractStatus(){
+        return $this->belongsTo(EmployeeContractStatus::class);
+    }
+
+    public function employeePosition(){
+        return $this->belongsTo(EmployeePosition::class);
+    }
+
+}
