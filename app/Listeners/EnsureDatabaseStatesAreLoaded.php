62683609648c34fa72778549858530abaa0e10b4
Elisio Leonardo <elisio.leonardo@gmail.com>
2023-03-22 08:14:48 +0200

    Code Style Formating

--- app/Listeners/EnsureDatabaseStatesAreLoaded.php
+++ app/Listeners/EnsureDatabaseStatesAreLoaded.php
@@ -22,6 +22,6 @@ class EnsureDatabaseStatesAreLoaded
     public function handle(MigrationsEnded $event)
     {
         Artisan::call('ensure-database-state-is-loaded');
-        Artisan::call('import:vendas');
+        //Artisan::call('import:vendas');
     }
 }
