4973c42e9e8c35c1bfbb4f9f106d0cbf7054cb3a
Elisio Leonardo <elisio.leonardo@gmail.com>
2023-03-30 13:31:39 +0200

    Usando Model Events para gerar Employee Code

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+set('application', 'intranet');
+set('repository', 'git@github.com:backstageel/xava.git');
+set('keep_releases', 3);
+
+// Shared files/dirs between deploys
+add('shared_files', [
+    '.env',
+    'public/.user.ini',
+]);
+add('shared_dirs', [
+    'storage',
+    'bootstrap/cache',
+]);
+
+// Writable dirs by web server
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+]);
+
+// Hosts
+
+host('intranet.xava.co.mz')
+    ->set('remote_user', 'root')
+    ->set('writable_use_sudo', true)
+    ->set('deploy_path', '/var/www/{{application}}');
+
+
+desc('Runs the database migrations');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
