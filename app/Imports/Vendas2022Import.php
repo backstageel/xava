08921a748ee5eb0f525e38ba2ad8e6d094bb233a
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-06-23 09:52:09 +0200

    teste II

--- app/Imports/Vendas2022Import.php
+++ app/Imports/Vendas2022Import.php
@@ -103,7 +103,7 @@ class Vendas2022Import implements OnEachRow, WithHeadingRow, WithCalculatedFormu
         $product = Product::firstOrCreate([
             'name' => $row['descricao_duto'],
         ], ['sale_price' => $row['preco_venda'],],
-            ['purchase_price' => $row['PRECO DE COMPRA'],]
+            ['purchase_price' => $row['PRECO_DE_COMPRA'],]
         );
         $invoiceItem = SaleItem::firstOrCreate([
             'sale_id' => $invoice->id,
