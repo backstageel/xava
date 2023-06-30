a7cf593be70c2440bea69eb19a5f68db9f35488d
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-06-30 10:37:37 +0200

    changes dashbord_vendas

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -140,4 +140,110 @@
             });
         });
     </script>
+    <script>
+        $(function () {
+            "use strict";
+
+
+
+// chart 2
+
+            var ctx = document.getElementById("chart2").getContext('2d');
+
+            var gradientStroke1 = ctx.createLinearGradient(0, 0, 0, 300);
+            gradientStroke1.addColorStop(0, '#fc4a1a');
+            gradientStroke1.addColorStop(1, '#f7b733');
+
+            var gradientStroke2 = ctx.createLinearGradient(0, 0, 0, 300);
+            gradientStroke2.addColorStop(0, '#4776e6');
+            gradientStroke2.addColorStop(1, '#8e54e9');
+
+
+            var gradientStroke3 = ctx.createLinearGradient(0, 0, 0, 300);
+            gradientStroke3.addColorStop(0, '#ee0979');
+            gradientStroke3.addColorStop(1, '#ff6a00');
+
+            var gradientStroke4 = ctx.createLinearGradient(0, 0, 0, 300);
+            gradientStroke4.addColorStop(0, '#42e695');
+            gradientStroke4.addColorStop(1, '#3bb2b8');
+
+            var myChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: @json("Pago"),
+                    datasets: [{
+                        backgroundColor: [
+                            gradientStroke1,
+                            gradientStroke2,
+                            gradientStroke3,
+                            gradientStroke4
+                        ],
+                        hoverBackgroundColor: [
+                            gradientStroke1,
+                            gradientStroke2,
+                            gradientStroke3,
+                            gradientStroke4
+                        ],
+                        data: @json($sale_pago, $sale_facturado, $sale_draft, $sale_cotacao),
+                        borderWidth: [1, 1, 1, 1]
+                    }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    cutoutPercentage: 75,
+                    legend: {
+                        position: 'bottom',
+                        display: false,
+                        labels: {
+                            boxWidth: 8
+                        }
+                    },
+                    tooltips: {
+                        displayColors: false,
+                    }
+                }
+            });
+
+
+// worl map
+
+            jQuery('#geographic-map-2').vectorMap(
+                {
+                    map: 'world_mill_en',
+                    backgroundColor: 'transparent',
+                    borderColor: '#818181',
+                    borderOpacity: 0.25,
+                    borderWidth: 1,
+                    zoomOnScroll: false,
+                    color: '#009efb',
+                    regionStyle: {
+                        initial: {
+                            fill: '#008cff'
+                        }
+                    },
+                    markerStyle: {
+                        initial: {
+                            r: 9,
+                            'fill': '#fff',
+                            'fill-opacity': 1,
+                            'stroke': '#000',
+                            'stroke-width': 5,
+                            'stroke-opacity': 0.4
+                        },
+                    },
+                    enableZoom: true,
+                    hoverColor: '#009efb',
+                    markers: [{
+                        latLng: [21.00, 78.00],
+                        name: 'Lorem Ipsum Dollar'
+
+                    }],
+                    hoverOpacity: null,
+                    normalizeFunction: 'linear',
+                    scaleColors: ['#b6d6ff', '#005ace'],
+                    selectedColor: '#c9dfaf',
+                    selectedRegions: [],
+                    showTooltip: true,
+                });
+
 @endsection
