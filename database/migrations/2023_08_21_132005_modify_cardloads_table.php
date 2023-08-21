3ddcb4ad75e6458f55a920dfe7a937952dc695a8
isaias.manjate <isaias.naftal.manjate@gmail.com>
2023-08-21 15:25:42 +0200

    fixing bugs

---
+++ database/migrations/2023_08_21_132005_modify_cardloads_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('card_loads', function (Blueprint $table){
+
+
+            $table->dropColumn('value')->change();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
